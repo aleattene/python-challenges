c8cbfa6c87b81a6fc9d0b6c7c9cceca2d2246e4d
aleattene <aleattene@gmail.com>
2025-08-10 12:12:47 +0200

    chore(aoc): fix tests day 01/2023

--- advent_of_code/year_2023/day_01_trebuchet/test_day_01_2023.py
+++ advent_of_code/year_2023/day_01_trebuchet/test_day_01_2023.py
@@ -1,6 +1,11 @@
 import os
+from dotenv import load_dotenv
 from .solution_one_day_01_2023 import solve_day_01_2023_one
 from .solution_two_day_01_2023 import solve_day_01_2023_two
+
+
+load_dotenv()
+environment = os.getenv("ENVIRONMENT")
 
 filename_demo_one = "input_demo_one.txt"
 filename_demo_two = "input_demo_two.txt"
@@ -11,16 +16,15 @@ file_path_demo_two = os.path.join(current_dir, filename_demo_two)
 file_path = os.path.join(current_dir, filename)
 
 
-def test_day_01_2023_one():
-    result_demo_one = solve_day_01_2023_one(file_path_demo_one)
-    assert result_demo_one == 142
-    result_one = solve_day_01_2023_one(file_path)
-    assert result_one == 56397
-
-
-def test_second_part():
-    result_demo_two = solve_day_01_2023_two(file_path_demo_two)
-    assert result_demo_two == 281
-    result_two = solve_day_01_2023_two(file_path)
-    assert result_two == 55701
-
+def test_day_01_2023():
+    result_demo_1 = solve_day_01_2023_one(file_path_demo_one)
+    assert result_demo_1 == 142
+    result_demo_2 = solve_day_01_2023_two(file_path_demo_two)
+    assert result_demo_2 == 281
+    if environment == "development":
+        expected_result_1 = int(os.getenv("SOLUTION_01_DAY_01_2023"))
+        result_1 = solve_day_01_2023_one(file_path)
+        assert expected_result_1 == result_1
+        expected_result_2 = int(os.getenv("SOLUTION_02_DAY_01_2023"))
+        result_2 = solve_day_01_2023_two(file_path)
+        assert expected_result_2 == result_2
