5aed1e06c0550a392f364af4dd53815fddaa1bfc
aleattene <aleattene@gmail.com>
2025-08-10 12:16:58 +0200

    chore(aoc): fix tests day 03/2023

--- advent_of_code/year_2023/day_03_gear_ratios/test_day_03_2023.py
+++ advent_of_code/year_2023/day_03_gear_ratios/test_day_03_2023.py
@@ -1,5 +1,9 @@
 import os
+from dotenv import load_dotenv
 from .solution_day_03_2023 import solve_day_03_2023
+
+load_dotenv()
+environment = os.getenv("ENVIRONMENT")
 
 filename_demo = "input_demo.txt"
 filename = "input.txt"
@@ -11,8 +15,10 @@ file_path = os.path.join(current_dir, filename)
 def test_day_03_2023():
     result_demo = solve_day_03_2023(file_path_demo)
     assert result_demo == (4361, 467835)
-    result = solve_day_03_2023(file_path)
-    assert result == (519444, 74528807)
-
+    if environment == "development":
+        expected_results = (int(os.getenv("SOLUTION_01_DAY_03_2023")),
+                            int(os.getenv("SOLUTION_02_DAY_03_2023")))
+        results = solve_day_03_2023(file_path)
+        assert expected_results == results
 
 
